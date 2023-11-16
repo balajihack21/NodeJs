34d48f4cf9c9f12d94f4a89ff8b899812fa9a67f
balajihack21 <balajiwebhost@gmail.com>
2023-11-16 21:20:35 +0530

    Revise JS

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log("Hello world")
+// console.log("Hello world")
+
+
+
+var prod=(a,b)=>{
+    return a*b
+}
+console.log(prod(10,20))
+
+
+//object
+let obj={
+    name:"Alexa",
+    age:"25",
+    greet(){
+        console.log("Hi ,I am "+this.name)
+    }
+}
+
+obj.greet()
